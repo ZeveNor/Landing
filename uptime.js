62ee5e9a28ed5631b4f3a9282c65523e87410aa9
ZeveNor <panthep094@gmail.com>
2025-04-01 23:54:35 +0700

    update chart

--- uptime.js
+++ uptime.js
@@ -55,30 +55,3 @@ document.addEventListener("DOMContentLoaded", function () {
   timelineElement.appendChild(container);
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const timelineElement = document.getElementById("timeline-Scraper");
-  timelineElement.innerHTML = "";
-  const container = document.createElement("div");
-  container.className = "status-squares d-flex flex-row-reverse";
-
-  const today = new Date();
-  today.setHours(0, 0, 0, 0);
-
-  for (let i = 0; i < 50; i++) {
-    const date = new Date(today); 
-    date.setDate(date.getDate() - i);
-    const dateString = date.toISOString().split("T")[0];
-
-    const statusSquare = document.createElement("div");
-    statusSquare.className = "status-square";
-
-    const isUp = Math.random() > 0;
-    
-    statusSquare.classList.add(isUp ? "bg-green" : "bg-red");
-    statusSquare.setAttribute("title", `${date.toLocaleDateString()}: ${isUp ? 'Operational' : 'Down'}`);
-
-    container.appendChild(statusSquare);
-  }
-
-  timelineElement.appendChild(container);
-});
